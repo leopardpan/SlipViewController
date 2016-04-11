c06e35b5fab76983f8731e3c30ca9d540e8ce2a8
leopardpan <panbaixin123@163.com>
2016-04-11 22:19:10 +0800

    init

--- ScrollDemo/ViewController.m
+++ ScrollDemo/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "SwipableViewController.h"
+
+#import "ViewController1.h"
+#import "ViewController2.h"
+#import "ViewController3.h"
+#import "ViewController4.h"
 
 @interface ViewController ()
 
@@ -16,7 +22,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self enter];
+    // Do any ad;ditional setup after loading the view, typically from a nib.
+}
+
+- (void)enter {
+
+    
+    ViewController1 *vc1 = [[ViewController1 alloc] init];
+    ViewController2 *vc2 = [[ViewController2 alloc] init];
+    ViewController3 *vc3 = [[ViewController3 alloc] init];
+    ViewController4 *vc4 = [[ViewController4 alloc] init];
+    
+    vc1.view.backgroundColor  = [UIColor greenColor];
+    vc2.view.backgroundColor  = [UIColor yellowColor];
+    vc3.view.backgroundColor  = [UIColor redColor];
+    vc4.view.backgroundColor  = [UIColor blueColor];
+    
+    SwipableViewController *newsSVC = [[SwipableViewController alloc] initWithTitle:@"测试"
+                                                                       andSubTitles:@[@"资讯", @"热点", @"博客", @"推荐"]
+                                                                     andControllers:@[vc1, vc2, vc3,vc4]
+                                                                        underTabbar:NO];
+    [self.navigationController pushViewController:newsSVC animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
